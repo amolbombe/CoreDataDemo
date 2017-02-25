1e9e166e57a7b3c45db25c1fb8b088c46a65ee6c
Amol Bombe <amolbombe@Amols-MacBook-Air.local>
2017-02-25 16:22:04 +0530

    Initial Commit

---
+++ CodeDataDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeDataDemo
+//
+//  Created by Amol Bombe on 25/02/17.
+//  Copyright © 2017 Amol Bombe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
